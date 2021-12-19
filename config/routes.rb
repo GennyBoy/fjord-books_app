000b9b46dd23cdfac88d044ad89720053962946b
GennyBoy <tassie2856@gmail.com>
2021-12-19 17:46:00 +0900

    ユーザー詳細画面にユーザーの情報を表示できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
   resources :books
+  devise_scope :user do
+    get '/users/:id', to: 'users/registrations#show'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'books#index'
