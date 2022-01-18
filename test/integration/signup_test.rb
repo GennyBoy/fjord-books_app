76cef0cc5c4a17a1371e0bb72b579a93a5484870
GennyBoy <tassie2856@gmail.com>
2022-01-18 21:58:15 +0900

    アイコン画像アップロードを含むサインアップのintegration testを追加

---
+++ test/integration/signup_test.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class SignupTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  def after_teardown
+    super
+    FileUtils.rm_rf(ActiveStorage::Blob.service.root)
+  end
+
+  test 'can signup' do
+    assert_difference('User.count', 1) do
+      post user_registration_path,
+           params: {
+             user: {
+               name: 'test user',
+               avatar: fixture_file_upload('david.png', 'image/png'),
+               email: 'test3@example.com',
+               password: 'password',
+               password_confirmation: 'password',
+             }
+           }
+    end
+    user = User.order(:created_at).last
+    # 今回のプラクティスの要件のテスト
+    assert user.avatar.attached?
+  end
+end
