56a1e598494b5b388100ba7e67ffebea1cbd3c65
GennyBoy <tassie2856@gmail.com>
2022-01-20 23:29:06 +0900

    render の引数の : を削除した

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -3,11 +3,11 @@
 class Users::UsersController < ApplicationController
   def index
     @users = User.order(:id).page(params[:page])
-    render :'users/index'
+    render 'users/index'
   end
 
   def show
     @user = User.find_by(id: params[:id])
-    render :'users/show'
+    render 'users/show'
   end
 end
