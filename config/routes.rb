5c8cea413d786db634052c441d2530a05354725a
GennyBoy <tassie2856@gmail.com>
2022-01-21 20:59:06 +0900

    devise_scope の使い方を間違えていたので修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,10 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
   resources :books
+  resources :users, only: %i(index show)
   devise_scope :user do
-    get '/users/:id', to: 'users/users#show', as: :user
-    get '/users', to: 'users/users#index'
+    get 'sign_in', to: 'users/sessions#new'
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'books#index'
 
