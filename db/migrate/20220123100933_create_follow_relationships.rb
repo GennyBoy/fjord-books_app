1afa9d9e4866c735a511d501fb2a1ea120bd326b
GennyBoy <tassie2856@gmail.com>
2022-01-23 19:21:31 +0900

    インデックスを追加して rails db:migrate

--- db/migrate/20220123100933_create_follow_relationships.rb
+++ db/migrate/20220123100933_create_follow_relationships.rb
@@ -6,5 +6,8 @@ class CreateFollowRelationships < ActiveRecord::Migration[6.1]
 
       t.timestamps
     end
+    add_index :follow_relationships, :follower_id
+    add_index :follow_relationships, :followed_id
+    add_index :follow_relationships, %i[follower_id followed_id], unique: true
   end
 end
