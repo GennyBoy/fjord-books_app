367e18ad1baca3bc8ef619bdfc9ef6f8534880c1
GennyBoy <tassie2856@gmail.com>
2022-01-29 17:44:05 +0900

    User/FollowRelationship の関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+  has_many :follow_relationships, class_name: 'FollowRelationship',
+                                  foreign_key: 'follower_id',
+                                  dependent: :destroy
 end
