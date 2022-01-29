227aacfa13507453f4217ec86b6da404e2cb48e8
GennyBoy <tassie2856@gmail.com>
2022-01-29 20:53:12 +0900

    User モデルに following の関連付けを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
   has_many :follow_relationships, class_name: 'FollowRelationship',
                                   foreign_key: 'follower_id',
                                   dependent: :destroy
+  has_many :following, through: :follow_relationships, source: :followed
 end
