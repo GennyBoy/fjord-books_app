33d764769c1e4227bdfcd471f750720df60a30f8
GennyBoy <tassie2856@gmail.com>
2022-02-11 19:15:19 +0900

    ページのタイトルをグローバル変数として定義するのをやめた

--- app/controllers/follow_relationships_controller.rb
+++ app/controllers/follow_relationships_controller.rb
@@ -2,14 +2,12 @@
 
 class FollowRelationshipsController < ApplicationController
   def following
-    @title = 'フォロー中'
     @user = User.find(params[:id])
     @users = @user.following.order(:id).page(params[:page])
     render 'users/show_follow'
   end
 
   def followers
-    @title = 'フォロワー'
     @user = User.find(params[:id])
     @users = @user.followers.order(:id).page(params[:page])
     render 'users/show_follow'
