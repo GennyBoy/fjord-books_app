fda131d409ee3ea709d9e183ab32505b257fc437
GennyBoy <tassie2856@gmail.com>
2022-02-26 21:14:53 +0900

    migration ファイルに FK と index を追加

--- db/migrate/20220226115235_create_reports.rb
+++ db/migrate/20220226115235_create_reports.rb
@@ -3,8 +3,11 @@ class CreateReports < ActiveRecord::Migration[6.1]
     create_table :reports do |t|
       t.string :title
       t.string :content
+      t.integer :user_id, null: false
 
       t.timestamps
     end
+    add_foreign_key :reports, :users
+    add_index :reports, :user_id
   end
 end
