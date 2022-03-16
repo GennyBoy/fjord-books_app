4f04993b1e01e48da955888e6faa9f40cbd411ee
GennyBoy <tassie2856@gmail.com>
2022-03-17 05:42:22 +0900

    日報詳細画面に投稿者と投稿日を表示するようにした

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,4 +2,13 @@
 
 class Report < ApplicationRecord
   belongs_to :user
+
+  def user
+    User.find(user_id)
+  end
+
+  def formatted_created_datetime
+    day_of_week = %w[日 月 火 水 木 金 土]
+    created_at.strftime("%Y年%m月%d日(#{day_of_week[created_at.wday]}) %H:%M")
+  end
 end
