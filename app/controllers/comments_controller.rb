a29381cb3c485a5a232687357442dbbcb52b4857
GennyBoy <tassie2856@gmail.com>
2022-03-27 07:51:39 +0900

    commentsのアクションを共通化した

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+class CommentsController < ApplicationController
+  before_action :set_comment, only: %i[destroy edit update]
+  before_action :set_commentable, only: %i[create edit destroy update]
+  before_action :assert_creator_is_current_user, only: %i[destroy edit update]
+
+  def index
+    @comments = Comment.all
+  end
+
+  def edit; end
+
+  def create
+    @comment = @commentable.comments.build(comment_params)
+
+    if @comment.save
+      redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    else
+      redirect_to @commentable, notice: 'エラーが発生したため保存に失敗しました。'
+    end
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to @commentable, notice: t('controllers.common.notice_update', name: Comment.model_name.human)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to @commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+  end
+
+  private
+
+  def assert_creator_is_current_user
+    redirect_to @commentable, notice: t('errors.messages.forbidden_action_for_others') unless @comment.user == current_user
+  end
+
+  def set_commentable
+    @commentable =
+      if params[:book_id]
+        Book.find(params[:book_id])
+      elsif params[:report_id]
+        Report.find(params[:report_id])
+      end
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content).merge({ user_id: current_user.id })
+  end
+end
