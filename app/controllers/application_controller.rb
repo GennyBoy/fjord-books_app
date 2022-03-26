8f8314cd20625a4d27a8e7ea32ba8cb94b50d242
GennyBoy <tassie2856@gmail.com>
2022-03-27 08:21:02 +0900

    存在しないIDにアクセスしようとした場合に404になるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   protected
 
@@ -20,6 +21,10 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
+  end
+
+  def record_not_found
+    render plain: "404 Not Found", status: 404
   end
 
   def signed_in_root_path(_resource_or_scope)
