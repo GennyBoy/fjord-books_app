0aa10334c8e0d8535b19ac6bed99c84eb11c8086
GennyBoy <tassie2856@gmail.com>
2022-03-28 05:48:48 +0900

    ユーザーと関連付けられているので、これらのメソッドがなくてもユーザーを取得できることがわかった

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,8 +4,4 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
   validates :title, :user_id, presence: true
-
-  def user
-    User.find(user_id)
-  end
 end
