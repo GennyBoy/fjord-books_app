b885967be591f77e5a0849dfabe6f55bcace0a37
GennyBoy <tassie2856@gmail.com>
2022-04-02 12:55:13 +0900

    今回の対応すべき内容とは関係ない変更だったので削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   protected
 
@@ -21,10 +20,6 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
-  end
-
-  def record_not_found
-    render plain: '404 Not Found', status: :not_found
   end
 
   def signed_in_root_path(_resource_or_scope)
