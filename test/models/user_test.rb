fef4069a9697e3d460fd9cd491f7cd0197271187
GennyBoy <tassie2856@gmail.com>
2022-04-17 08:55:05 +0900

    データ準備を共通化

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,40 +3,34 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test '#following?' do
-    me = User.create!(email: 'me@example.com', password: 'password')
-    she = User.create!(email: 'she@example.com', password: 'password')
+  setup do
+    @me = users(:bob)
+    @she = users(:alice)
+  end
 
-    assert_not me.following?(she)
-    me.follow(she)
-    assert me.following?(she)
+  test '#following?' do
+    assert_not @me.following?(@she)
+    @me.follow(@she)
+    assert @me.following?(@she)
   end
 
   test '#followed_by?' do
-    me = User.create!(email: 'me@example.com', password: 'password')
-    she = User.create!(email: 'she@example.com', password: 'password')
-
-    assert_not she.followed_by?(me)
-    me.follow(she)
-    assert she.followed_by?(me)
+    assert_not @she.followed_by?(@me)
+    @me.follow(@she)
+    assert @she.followed_by?(@me)
   end
 
   test '#follow' do
-    me = User.create!(email: 'me@example.com', password: 'password')
-    she = User.create!(email: 'she@example.com', password: 'password')
-
-    assert_not me.following?(she)
-    me.follow(she)
-    assert me.following?(she)
+    assert_not @me.following?(@she)
+    @me.follow(@she)
+    assert @me.following?(@she)
   end
 
   test '#unfollow' do
-    me = User.create!(email: 'me@example.com', password: 'password')
-    she = User.create!(email: 'she@example.com', password: 'password')
-    me.follow(she)
+    @me.follow(@she)
 
-    me.unfollow(she)
-    assert_not me.following?(she)
+    @me.unfollow(@she)
+    assert_not @me.following?(@she)
   end
 
   test '#name_or_email' do
