3c3c955dd5ddd9c1b7beec7f7c5f115e92f84b02
GennyBoy <tassie2856@gmail.com>
2022-04-17 14:44:56 +0900

    Reportモデルのテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,25 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @me = users(:bob)
+    @my_report = reports(:report_by_bob)
+    @she = users(:alice)
+    @her_report = reports(:report_by_alice)
+  end
+
+  test "#editable? returns true when target user == the report's user" do
+    assert @my_report.editable?(@me)
+  end
+
+  test "#editable? returns false when target user != the report's user" do
+    assert_not @her_report.editable?(@me)
+  end
+
+  test '#created_on returns Date class' do
+    # created_at のままだと Dateクラス ではないことを一応確認
+    assert_not_instance_of Date, @my_report.created_at
+
+    assert_instance_of Date, @my_report.created_on
+  end
 end
